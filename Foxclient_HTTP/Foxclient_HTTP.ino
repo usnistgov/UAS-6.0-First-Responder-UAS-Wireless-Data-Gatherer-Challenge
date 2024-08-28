846bf4c88b10427783289030a0f9d7c60e8d4bb7
dharriss1 <65728924+dharriss1@users.noreply.github.com>
2024-08-28 12:18:21 -0600

    Initial Upload

---
+++ Foxclient_HTTP/Foxclient_HTTP.ino
@@ -0,0 +1,145 @@
+/*
+ * NIST-PSCR Chic.Odell@nist.gov - UAS 6.0 Prize Challenge - "Fox-Node" HTTP IoT Client  
+ * https://docs.espressif.com/projects/esp-idf/en/latest/esp32s2/index.html
+ * https://github.com/espressif/arduino-esp32
+ * https://datatracker.ietf.org/doc/html/rfc2616 
+ * https://arduinojson.org/v7/how-to/use-arduinojson-with-httpclient/ 
+ */
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <HTTPClient.h>
+
+const char* serverName = "http://192.168.1.1/update";               // URI for POST Server  
+const int httpPort = 80;                                            // def port assignment 
+const char* ssid = "UAS 6.0 ";                                      // UAS 6.0 Test Network SSID
+const char* password = "UAS 6.0 ";                                  // UAS 6.0 Test Network PSW
+char* http_payload = "NULL";                                        // Var for HTTP payload storage
+long pkt_count = 0;                                                 // Client packet counter
+int httpResponseCode = -1;                                          // Client resp codes
+String httpDummyData = "Null";                                      // Client Payload var
+
+                                                                    // Includes for display
+#include <Adafruit_GFX.h>                                           // Display hardware def.
+#include <Adafruit_ST7789.h>                                        // Hardware-specific library for ST7789
+#include <SPI.h>
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);     // Use dedicated hardware SPI pins for TFT disp
+
+#include "ArduinoJson.h"
+DynamicJsonDocument doc(2048);                                      // Create an empty document
+
+unsigned long lastTime = 0;             // easy mode time var setup
+unsigned long timerDelay = 10000;        // Timer set to 1 seconds (10000) Update this var for POST msg loop timing
+
+void tft_display(int disp_update){ 
+/*disp key 
+ 3 - on sent POST (GREEN)
+ 2 - no connection to HTTP server (Orange)
+ 1 - On WiFi got IP 
+ 0 - TFT init code (only call during setup)
+*/
+ short disp_rotation = 1;                     // update to rotate TFT display: input "1" through "4"                   
+  if (disp_update ==3){
+    tft.fillScreen(ST77XX_GREEN);tft.setCursor(0, 0);
+    tft.setTextSize(3);tft.setTextColor(ST77XX_WHITE);tft.println("FOX > UAS 6.0");
+    tft.setTextSize(2);tft.setTextColor(ST77XX_RED);tft.setTextSize(2);tft.print("IP: ");tft.println(WiFi.localIP());
+    tft.print("WiFi RSSI: ");tft.println(WiFi.RSSI()); // THIS CALL SEEMS NOT TO WORK 
+    tft.print("Sent HTTP POST# ");tft.println(pkt_count);
+    tft.println("Payload: ");tft.println(httpDummyData);
+    tft.println("HTTP Response code: ");tft.println(httpResponseCode);
+    tft.setTextColor(ST77XX_WHITE);             // Set back to white for later calls
+  }else if(disp_update == 2){                   // no HTTP Server connection aval
+    tft.fillScreen(ST77XX_ORANGE);tft.setCursor(0, 0);
+    tft.setTextSize(3);tft.setTextColor(ST77XX_WHITE);tft.println(("FOX > UAS 6.0"));         
+    tft.setTextSize(2);tft.println("HTTP Server Connection Failed.");
+    tft.println("");tft.println("Reconnteting...");
+    tft.println("Host URI:  ");tft.println(serverName);
+  }else if(disp_update == 1){                  // (BLUE) disp IP connected
+    tft.fillScreen(ST77XX_BLUE);tft.setCursor(0, 0);
+    tft.setTextSize(3);tft.setTextColor(ST77XX_WHITE);tft.println("FOX> UAS 6.0");
+    tft.setTextSize(2);tft.println("WiFi Connected.");
+    tft.print("IP: ");tft.println(WiFi.localIP());
+  }else if(disp_update == 0){                  // (GREEN) disp initial setup  screen
+    pinMode(TFT_BACKLITE, OUTPUT);             // Start TFT &turn on backlite
+    digitalWrite(TFT_BACKLITE, HIGH);
+    tft.init(135, 240);                         // ST7789 (TFT display) 240x135
+    tft.setRotation(disp_rotation);tft.setCursor(0, 0);tft.fillScreen(ST77XX_GREEN);
+    tft.setTextSize(3);tft.setTextColor(ST77XX_WHITE);tft.println("FOX> UAS 6.0");tft.println("INITIALIZED");
+    tft.setTextSize(2);                         // End TFT init
+  }else{                                        //TODO err message/handeling here? 
+    Serial.println("Error in display function...");  
+    Serial.println("tft_display(");Serial.println(disp_update,DEC);Serial.print(")");
+  }
+}
+
+// start of WiFi Callbacks (for network reconnection)
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){  
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.println("WiFi connected"); 
+  WiFi.setTxPower(WIFI_POWER_7dBm);              // Set Tx power low (for Stage 2 this setting works well) NOTE: power def --> https://github.com/espressif/arduino-esp32/blob/70786dc5fa51601f525496d0f92a220c917b4ad9/libraries/WiFi/src/WiFiGeneric.h#L47
+  Serial.print("WiFi Tx Power: ");Serial.println(WiFi.getTxPower());  // Check pwr setting and send to Serial
+  Serial.println("IP address: ");Serial.println(WiFi.localIP());
+  tft_display(1);                                // disp connection info
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
+  Serial.print("WiFi Station: ");Serial.print(event);Serial.print(" Reason: ");Serial.println(info.wifi_sta_disconnected.reason);
+  Serial.println("Trying to Reconnect");
+  WiFi.disconnect();                             
+  WiFi.reconnect();                              
+  tft_display(2);                                // disp disconnection message
+}
+// End of WiFi Callbacks 
+
+void setup() { 
+  Serial.begin(115200);        // Start Serial interface
+  WiFi.disconnect(true);       // init WiFi
+  tft_display(0);              // init TFT for ESP32 dispaly
+  delay(1000);
+
+  WiFi.onEvent(WiFiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);              // Implement WiFi Callback functions to avoid handles in main loop 
+  WiFi.onEvent(WiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);                            
+  WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);        // Handle WiFi reconnect 
+  WiFi.begin(ssid, password);
+  Serial.println("**Fox-Node Initialized**");
+}
+
+void loop() 
+{
+  if ((millis() - lastTime) > timerDelay) {       // Try to send a HTTP-POST every "timerDelay" [10 s]
+    if(WiFi.status()== WL_CONNECTED){             //Check WiFi connection status
+      Serial.println("Attempting POST.");
+      HTTPClient http;
+      http.begin(serverName);Serial.println("Server connected, Sending POST.");
+      http.useHTTP10(true);                                             // Dont use chunking, shouldnt given our small payload but best to define... 
+      http.addHeader("Content-Type", "application/json");               // Specify content-type header JSON
+      doc["Node_RSSI"]=WiFi.RSSI();                                     // Read WiFi Connection info into JSON doc
+      serializeJson(doc,httpDummyData);                                 // create JSON HTTP-POST payload 
+      httpResponseCode = http.POST(httpDummyData);                      // Send HTTP-POST request
+      pkt_count = pkt_count + 1;                                        // Add to total packet count 
+      if (httpResponseCode == 200){Serial.println("POST success.");
+      }else if(httpResponseCode == HTTPC_ERROR_CONNECTION_REFUSED){     // NOTE: HTTPC_ERROR_CONNECTION_REFUSED == -1 https://github.com/espressif/arduino-esp32/blob/master/libraries/HTTPClient/src/HTTPClient.h
+        WiFi.reconnect();           // Debug #1 client gets stuck on IP network but fails HTTP POST... recover from this state w/ reconnect
+      }      
+
+      Serial.print("STA PWR: ");Serial.println(WiFi.getTxPower());      // Check WiFi Tx power / send to serial
+      Serial.print("HTTP Response code: ");Serial.println(httpResponseCode);
+      Serial.print("POST Payload: ");serializeJson(doc,Serial);         // Display responce info to serial
+      http.end();                                                       // Free HTTP resources
+      tft_display(3);                                                   // Disp HTTP session POST responce info   
+    }
+    else {
+      Serial.println("WiFi Disconnected");  // Network unavailable :ERR state WiFi will auto reconnect 
+      if ((WiFi.status() != WL_CONNECTED) && (millis() - lastTime) > timerDelay) { // If timer is up and no server, reconnect
+        WiFi.reconnect();                                               // TODO: call WiFi disconnect ISR manually
+        tft_display(2);                                                 // disp no connection screen 
+      } 
+    }
+    lastTime = millis();                    // Reset our counter
+  }
+}
+
+
---
+++ README.md
@@ -0,0 +1,120 @@
+# [UAS 6.0 First Responder UAS Wireless Data Gatherer Challenge](https://www.nist.gov/ctl/pscr/open-innovation-prize-challenges/current-and-upcoming-prize-challenges/2024-first-responder)
+
+## Scope
+This repository contains the IoT-Client software for the NIST-PSCR UAS6.0 Prize Challenge "**Fox Hunt**" Stage 2 & 3 events. These IoT nodes will be referred to herein as "Fox-Node(s)".
+
+**Stage 2**: IoT addition to the NIST defined [Open Test Lanes and Scenarios](https://www.nist.gov/el/intelligent-systems-division-73500/standard-test-methods-response-robots/aerial-systems/open-test) UAS test bed. 
+
+**Stage 3**: 
+UAS prize challenge competitors will participate in a flight challenge to find and download data from nodes scattered throughout the competition theater. 
+
+## Fox-Node Solution Components
+**Hardware**: 
+- ESP32-S2 ([Adafruit ESP32-S2 TFT Feather](https://learn.adafruit.com/adafruit-esp32-s2-tft-feather/overview))
+- [Battery pack](https://www.ravpower.com/products/ravpower-10000mah-power-bank-dual-outputs?_pos=1&_psq=prime+1000&_ss=e&_v=1.0) (USB-C out to ESP32)
+- USB to USB-C cable (Battery --> ESP32)
+
+NOTE: The ESP32 family of embedded microcontrollers has many different purchasing options that often use different/incompatible displays, buss connections, perifierals, ect. To avoid confusion is it highly advisable to purchase the specific ESP32 "flavor" mentioned above for best compatibility with the provided source code. 
+
+**Software**: 
+- Host PC to program target IoT Fox-Node(s)
+    - [Arduino IDE ](https://www.arduino.cc/en/software)
+
+## Installation & Setup
+- Install the [Arduino IDE ](https://www.arduino.cc/en/software) on target "host PC" to build project source code and program ESP32 hardware.
+    - ESP32 hardware support package dependancie listed below, install via Arduino IDE GUI "[Board Manager](https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-board-manager/)" 
+        - [esp32](https://github.com/espressif/arduino-esp32) by Espressif Systems <-- main "arduino-esp32" wrapped software stack
+    - Software dependancies libraries shown below, install via Arduino IDE GUI "[Library Manager](https://www.arduino.cc/en/Guide/Libraries/)" outside of the core Arduino-ESP32 software stack.
+        - [ArduinoJson](https://github.com/bblanchon/ArduinoJson) v7.1.0 by Benoit Blanchon
+        - [Adafruit ST7735 and ST7789 Library](https://github.com/adafruit/Adafruit-ST7735-Library) v1.10.4 by Adafruit (Select "Install All" option to bring in deps listed below)
+            - Adafruit BusIO v1.16.1            (dependency of Adafruit ST7735 and ST7789 installed along side by default.)
+            - Adafruit GFX Library v1.11.10     (dependency of Adafruit ST7735 and ST7789 installed along side by default.)
+            - Adafruit seesaw v1.7.8 Library    (dependency of Adafruit ST7735 and ST7789 installed along side by default.)
+            - SD v1.3.0 Library                 (dependency of Adafruit ST7735 and ST7789 installed along side by default.)
+
+With IDE setup completed the "Fox-Node to be" (ESP32 hardware) can now be connected to host PC via USB and programed with the provided "Foxclient_HTTP.ino" source code file via Arduino IDE. 
+
+# Usage
+The "Fox Hunt" project leverages the [Arduino IDE](https://www.arduino.cc/en/software), [Arduino ESP32](https://github.com/espressif/arduino-esp32) software stack and [ESP32 hardware](https://learn.adafruit.com/adafruit-esp32-s2-tft-feather/overview) to run a simple HTTP-Client. This client (Foxclient_HTTP.ino), referred to as a "Fox-Node" will auto-connect to the predefined WiFi credentials and beacon an HTTP-POST message every 10 seconds.
+
+NOTE: Target WiFi SSID/PSWD can be easily updated in the source. **Default SSID/PSWD** are both hardcoded as **"UAS 6.0 "**.
+
+![Fox Node State Diagram](pics/FoxNode_StateDiagram.png)
+
+Fox-Node verification:
+The Arduino IDE provides easy access to a serial interface connection (115200 baud) that will display runtime information. This information is also pushed/displayed via ESP TFT display (unique to the Adafruit ESP32-S2 TFT Feather). 
+
+Upon connection to "UAS 6.0 " WiFi Network serial output... ex: 
+
+```
+WiFi connected
+**Fox-Node Initialized**
+WiFi Tx Power: 28
+IP address: 
+192.168.1.2
+Attempting POST.
+Server connected, Sending POST.
+POST success.
+STA PWR: 28
+HTTP Response code: 200
+POST Payload: {"Node_RSSI":-38}
+```
+
+If the Fox-Node fails to find the specified WiFi Network... ex:
+
+```
+**Fox-Node Initialized**
+WiFi Station: 14 Reason: 201
+Trying to Reconnect
+WiFi Station: 14 Reason: 201
+Trying to Reconnect
+```
+
+NOTE: not all ESP32 hardware will have a display output. To make this project compatible with such hardware all references to the TFT display can be removed from the source Foxclient_HTTP.ino file to provide more "generic" ESP32 flavor compatibility.
+
+## ESP32 Display
+
+Fox-Node display states:
+
+<Disp. # >, description (background color)
+
+- 3, on sent POST,                   (GREEN)
+
+![Fox Node State Diagram](pics/FoxNode_disp3_tight.jpg){width=100 height=100px}
+
+- 2, no connection to HTTP server,   (Orange)
+
+![Fox Node State Diagram](pics/FoxNode_disp2_tight.jpg){width=100 height=100px}
+
+- 1, On WiFi got IP,                 (Blue)
+
+![Fox Node State Diagram](pics/FoxNode_disp1_tight.jpg){width=100 height=100px}
+
+- 0, TFT init code,                  (Green)
+
+![Fox Node State Diagram](pics/FoxNode_disp0_tight.jpg){width=100 height=100px}
+
+## Branch List 
+
+**main**: 
+- UAS 6.0 Stage 2 code (only dummy data + RSSI info from the Fox Node)
+
+**Live_Sensors**: 
+- Support for live data via I2C sensor board designed by Peter Hallenbeck (sending real sensor data as payload via I2C --> ESP --> HTTP-POST)
+
+## Project status
+Development targets PSCR - UAS 6.0 drone challenge 
+- Working Proof of concept w/ HTTP.POST "Fox-Nodes" w/ real time and dummy data.
+
+### Future Work  
+Implement:
+- NTP 
+- mDNS
+- LittleFS
+
+## License
+- Arduino ESP32 stack - LGPL-2.1 license
+    - ArduinoJson - MIT license
+
+## Authors and acknowledgment
+- Jordan O'Dell <chic.odell@nist.gov>
